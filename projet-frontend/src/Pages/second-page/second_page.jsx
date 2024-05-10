4e5adde775fa3e5b4060dceadb1be610d595f149
Djinaneb <155325279+Djinaneb@users.noreply.github.com>
2024-05-10 18:24:57 +0100

    push m3ndou hata m3na

--- projet-frontend/src/Pages/second-page/second_page.jsx
+++ projet-frontend/src/Pages/second-page/second_page.jsx
@@ -5,7 +5,20 @@ import Supprimer from "../../Componenets/Supprimer/Supprimer";
 import Error from "../../Componenets/error/error"
 import "./second_page.css";
 import Sidbar from "../../Componenets/Sidebar/Sidebar"
-const second_page = (props) => {
+import React, { useState, useEffect } from 'react';
+import ReactSpeedometer from 'react-d3-speedometer';
+
+const SecondPage = (props) => {
+  const [speed, setSpeed] = useState(55); // Initial value set to 55
+
+  useEffect(() => {
+      const interval = setInterval(() => {
+          // Update the speed to a random value between 0 and 100
+          setSpeed(Math.floor(Math.random() * 100));
+      }, 1000); // Updates every second
+
+      return () => clearInterval(interval); // Clean up the interval on component unmount
+  }, []);
        return (
 
         <div className="second"> 
@@ -14,6 +27,30 @@ const second_page = (props) => {
           <Error/>
            <Info_entreprise/>
            <Notifications/>
+           <ReactSpeedometer
+            maxValue={100}
+            value={speed}
+            needleColor="red"
+            startColor="green"
+            segments={10}
+            endColor="blue"
+            valueTextFontSize="0px"  // Masque le texte de la valeur
+            labelFontSize="0px" // Assure que les labels des segments sont également masqués
+            customSegmentStops={[0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100]}
+        
+            segmentLabels={[
+                { text: "", color: "#000", fontSize: "0px" },  // Textes vides pour les labels
+                { text: "", color: "#000", fontSize: "0px" },
+                { text: "", color: "#000", fontSize: "0px" },
+                { text: "", color: "#000", fontSize: "0px" },
+                { text: "", color: "#000", fontSize: "0px" },
+                { text: "", color: "#000", fontSize: "0px" },
+                { text: "", color: "#000", fontSize: "0px" },
+                { text: "", color: "#000", fontSize: "0px" },
+                { text: "", color: "#000", fontSize: "0px" },
+                { text: "", color: "#000", fontSize: "0px" }
+            ]}
+        />
            <Securiteform/>
            <Supprimer/>
            </div>
@@ -24,4 +61,4 @@ const second_page = (props) => {
       );
 }
  
-export default second_page;
+export default SecondPage;
